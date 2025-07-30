110e1ee7138c39a89903472430e6ecfad4170101
SeppeP <seppe.parys8@gmail.com>
2025-07-30 16:20:06 +0200

    2015 day 04

---
+++ 2015/04/part2.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const crypto = require('crypto');
+
+function solve(inputFile) {
+    const key = fs.readFileSync(inputFile, 'utf-8');
+
+    let hash;
+    let stringToHash;
+
+    let i = -1;
+
+    while(hash?.substring(0, 6) !== '000000') {
+        i++;
+        stringToHash = `${key}${i}`;
+        hash = crypto.createHash('md5').update(stringToHash).digest('hex');
+    }
+
+    return i;
+}
+
+if (require.main === module) {
+    console.log(solve(__dirname + '/input.txt'));
+}
+
+module.exports = solve;
